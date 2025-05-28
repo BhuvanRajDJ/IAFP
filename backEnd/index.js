6e8af58df50aa2d3cc9f88d68310352de7272222
BhuvanRajDJ <133094255+BhuvanRajDJ@users.noreply.github.com>
2025-05-29 00:14:50 +0530

    updated S_fetchAssignments

--- backEnd/index.js
+++ backEnd/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "YOUR_FRONTEND_RENDER_URL",
+    origin: "https://iafp.onrender.com",
   })
 );
 
