29bd9fc7ddb37e41b7ff46b6ef7df58cbc5af4ef
BhuvanRajDJ <133094255+BhuvanRajDJ@users.noreply.github.com>
2025-05-29 00:13:50 +0530

    updated S_fetchAssignments

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,35 +5,37 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const OpenAI = require("openai").default;
-require('./models/db');
+require("./models/db");
 const userRouter = require("./routes/userRouter");
 const assignmentRouter = require("./routes/assignmentRouter");
-
+const cors = require("cors");
 
 const uploadRoutes = require("./routes/uploadRoutes");
 const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "YOUR_FRONTEND_RENDER_URL",
+  })
+);
 
-
-app.use('/', userRouter);
-app.use("/",assignmentRouter);
+app.use("/", userRouter);
+app.use("/", assignmentRouter);
 app.use("/api", uploadRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const openai = new OpenAI({
-    apiKey: process.env.BEARER_TOKEN
-})
-
+  apiKey: process.env.BEARER_TOKEN,
+});
 
 app.listen(PORT, () => {
-console.log(`The server is running at http://localhost:${PORT}`);
+  console.log(`The server is running at http://localhost:${PORT}`);
 });
